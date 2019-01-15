c978032eae63a8e60481aa82d5fc46aa052e7be2
jjy <jjyruby@gmail.com>
2019-01-15 21:25:19 +0800

    refactor: rewrite ChainPath

--- src/chain_path.rs
+++ src/chain_path.rs
@@ -1,15 +1,15 @@
 use crate::KeyIndex;
 use std::fmt;
-use std::str::FromStr;
 
 const MASTER_SYMBOL: &str = "m";
 const HARDENED_SYMBOLS: [&str; 2] = ["H", "'"];
 const SEPARATOR: char = '/';
 
 #[derive(Clone, Debug)]
-pub struct PathError {
-    level: usize,
-    reason: String,
+pub enum SubPathError {
+    Invalid,
+    Blank,
+    KeyIndexOutOfRange,
 }
 
 /// ChainPath is used to describe BIP-32 KeyChain path.
@@ -18,73 +18,57 @@ pub struct PathError {
 ///
 /// ``` rust
 /// # extern crate hdwallet;
-/// use hdwallet::{ChainPath, KeyIndex};
+/// use hdwallet::{ChainPath, SubPath, KeyIndex};
 ///
-/// let chain_path = ChainPath::Node(
-///     Box::new(ChainPath::Node(
-///         Box::new(ChainPath::Root),
-///         KeyIndex::hardened_from_normalize_index(1).unwrap()
-///     )),
-///     KeyIndex::Normal(1)
-/// );
-/// assert_eq!(chain_path.to_string(), "m/2147483649H/1");
-/// assert_eq!(ChainPath::from("m/2147483649H/1"), chain_path);
+/// let chain_path = ChainPath::from("m/2147483649H/1".to_string())
+///     .iter()
+///     .collect::<Result<Vec<_>, _>>()
+///     .unwrap();
+/// assert_eq!(chain_path, vec![
+///     SubPath::Root,
+///     SubPath::Child(KeyIndex::hardened_from_normalize_index(1).unwrap()),
+///     SubPath::Child(KeyIndex::Normal(1))
+/// ]);
 /// ```
 #[derive(Debug, PartialEq, Eq)]
-pub enum ChainPath {
-    Root,
-    Node(Box<ChainPath>, KeyIndex),
-}
+pub struct ChainPath(String);
 
 impl ChainPath {
-    /// Convert ChainPath to string represent format
-    fn to_string(&self) -> String {
-        let mut path = self;
-        let mut path_levels: Vec<String> = Vec::new();
-        loop {
-            match path {
-                ChainPath::Root => {
-                    path_levels.push("m".into());
-                    break;
-                }
-                ChainPath::Node(parent_path, key_index) => {
-                    let s = match key_index {
-                        KeyIndex::Normal(i) => i.to_string(),
-                        KeyIndex::Hardened(i) => format!("{}H", i),
-                    };
-                    path_levels.push(s);
-                    path = parent_path;
-                }
-            }
-        }
-        path_levels.reverse();
-        path_levels.join("/")
+    /// An SubPath iterator over the ChainPath from Root to child keys.
+    pub fn iter(&self) -> impl Iterator<Item = Result<SubPath, SubPathError>> + '_ {
+        Iter(self.0.split_terminator(SEPARATOR))
+    }
+
+    pub fn into_string(self) -> String {
+        self.0
+    }
+
+    /// Convert ChainPath to &str represent format
+    fn to_string(&self) -> &str {
+        &self.0
     }
 }
 
-impl FromStr for ChainPath {
-    type Err = PathError;
-    /// Convert &str represent chain path to ChainPath
-    fn from_str(path: &str) -> Result<Self, Self::Err> {
-        let mut iter = path.split_terminator(SEPARATOR);
-        if iter.next() != Some(MASTER_SYMBOL) {
-            return Err(PathError {
-                level: 0,
-                reason: "Must start with 'm'".into(),
-            });
-        }
-        let mut chain_path = ChainPath::Root;
-        for (i, sub_path) in iter.enumerate() {
-            let level = i + 1;
-            let last_char = match sub_path.get((sub_path.len() - 1)..) {
-                Some(c) => c,
-                None => {
-                    return Err(PathError {
-                        level,
-                        reason: "Subpath can't be blank".into(),
-                    })
-                }
-            };
+#[derive(Debug, PartialEq, Eq)]
+pub enum SubPath {
+    Root,
+    Child(KeyIndex),
+}
+
+pub struct Iter<'a, I: Iterator<Item = &'a str>>(I);
+
+impl<'a, I: Iterator<Item = &'a str>> Iterator for Iter<'a, I> {
+    type Item = Result<SubPath, SubPathError>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|sub_path| {
+            if sub_path == MASTER_SYMBOL {
+                return Ok(SubPath::Root);
+            }
+            if sub_path.is_empty() {
+                return Err(SubPathError::Blank);
+            }
+            let last_char = &sub_path[(sub_path.len() - 1)..];
             let is_hardened = HARDENED_SYMBOLS.contains(&last_char);
             let key_index = {
                 let index_result = if is_hardened {
@@ -94,44 +78,21 @@ impl FromStr for ChainPath {
                 };
                 let index = match index_result {
                     Ok(index) => index,
-                    Err(_) => {
-                        return Err(PathError {
-                            level,
-                            reason: "Illegal key index format".into(),
-                        })
-                    }
+                    Err(_) => return Err(SubPathError::Invalid),
                 };
                 match KeyIndex::from_index(index) {
                     Ok(key_index) => key_index,
-                    Err(_) => {
-                        return Err(PathError {
-                            level,
-                            reason: "Key index out of range".into(),
-                        })
-                    }
+                    Err(_) => return Err(SubPathError::KeyIndexOutOfRange),
                 }
             };
-            chain_path = ChainPath::Node(Box::new(chain_path), key_index);
-        }
-        Ok(chain_path)
+            Ok(SubPath::Child(key_index))
+        })
     }
 }
 
 impl From<String> for ChainPath {
     fn from(path: String) -> Self {
-        ChainPath::from_str(&path).expect("into chain path")
-    }
-}
-
-impl From<&str> for ChainPath {
-    fn from(path: &str) -> Self {
-        ChainPath::from_str(path).expect("into chain path")
-    }
-}
-
-impl Into<String> for ChainPath {
-    fn into(self) -> String {
-        self.to_string()
+        ChainPath(path)
     }
 }
 
@@ -146,57 +107,56 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_build_chain_path() {
-        assert_eq!(ChainPath::Root.to_string(), "m");
+    fn test_chain_path() {
         assert_eq!(
-            ChainPath::Node(Box::new(ChainPath::Root), KeyIndex::Normal(1)).to_string(),
-            "m/1"
+            ChainPath::from("m".to_string())
+                .iter()
+                .collect::<Result<Vec<_>, _>>()
+                .unwrap(),
+            vec![SubPath::Root]
         );
         assert_eq!(
-            ChainPath::Node(
-                Box::new(ChainPath::Node(
-                    Box::new(ChainPath::Root),
-                    KeyIndex::hardened_from_normalize_index(1).unwrap()
-                )),
-                KeyIndex::Normal(1)
-            )
-            .to_string(),
-            "m/2147483649H/1"
-        );
-    }
-
-    #[test]
-    fn test_chain_path_from_str() {
-        assert_eq!(ChainPath::from("m"), ChainPath::Root);
-        assert_eq!(
-            ChainPath::from("m/1"),
-            ChainPath::Node(Box::new(ChainPath::Root), KeyIndex::Normal(1))
+            ChainPath::from("m/1".to_string())
+                .iter()
+                .collect::<Result<Vec<_>, _>>()
+                .unwrap(),
+            vec![SubPath::Root, SubPath::Child(KeyIndex::Normal(1))],
         );
         assert_eq!(
-            ChainPath::from("m/2147483649H/1"),
-            ChainPath::Node(
-                Box::new(ChainPath::Node(
-                    Box::new(ChainPath::Root),
-                    KeyIndex::hardened_from_normalize_index(1).unwrap()
-                )),
-                KeyIndex::Normal(1)
-            )
+            ChainPath::from("m/2147483649H/1".to_string())
+                .iter()
+                .collect::<Result<Vec<_>, _>>()
+                .unwrap(),
+            vec![
+                SubPath::Root,
+                SubPath::Child(KeyIndex::hardened_from_normalize_index(1).unwrap()),
+                SubPath::Child(KeyIndex::Normal(1))
+            ],
         );
         // alternative hardened key represent
         assert_eq!(
-            ChainPath::from("m/2147483649'/1"),
-            ChainPath::Node(
-                Box::new(ChainPath::Node(
-                    Box::new(ChainPath::Root),
-                    KeyIndex::hardened_from_normalize_index(1).unwrap()
-                )),
-                KeyIndex::Normal(1)
-            )
+            ChainPath::from("m/2147483649'/1".to_string())
+                .iter()
+                .collect::<Result<Vec<_>, _>>()
+                .unwrap(),
+            vec![
+                SubPath::Root,
+                SubPath::Child(KeyIndex::hardened_from_normalize_index(1).unwrap()),
+                SubPath::Child(KeyIndex::Normal(1))
+            ],
         );
         // from invalid string
-        assert!(ChainPath::from_str("m/2147483649h/1").is_err());
-        assert!(ChainPath::from_str("/2147483649H/1").is_err());
-        assert!(ChainPath::from_str("2147483649H/1").is_err());
-        assert!(ChainPath::from_str("a").is_err());
+        assert!(ChainPath::from("m/2147483649h/1".to_string())
+            .iter()
+            .collect::<Result<Vec<_>, _>>()
+            .is_err());
+        assert!(ChainPath::from("/2147483649H/1".to_string())
+            .iter()
+            .collect::<Result<Vec<_>, _>>()
+            .is_err());
+        assert!(ChainPath::from("a".to_string())
+            .iter()
+            .collect::<Result<Vec<_>, _>>()
+            .is_err());
     }
 }
