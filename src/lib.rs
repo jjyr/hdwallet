96270438de7649a0d0d96ce7ef22b3ad047a7129
jjy <jjyruby@gmail.com>
2019-01-18 12:36:00 +0800

    chore: update to rust 1.32.0 and remove numext-fixed-uint

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@ pub use crate::error::Error;
 pub use crate::key::{
     ChildKey, ChildPrivKey, ChildPubKey, ExtendedKey, ExtendedPrivKey, ExtendedPubKey, KeySeed,
 };
-pub use crate::key_chain::{DefaultKeyChain, KeyChain, KeyInfo};
+pub use crate::key_chain::{DefaultKeyChain, KeyChain};
 pub use crate::key_index::KeyIndex;
 pub use crate::serialize::Serialize;
 pub use secp256k1;
